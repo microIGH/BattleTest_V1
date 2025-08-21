ede5d0bf74622338e094ab311907ad6dfcc72538
ISRAEL GARCIA <micro4004@yahoo.com.mx>
2025-08-20 21:19:07 -0600

    Initial Commit

---
+++ BattleTest_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BattleTest_V1
+//
+//  Created by ISRAEL GARCIA on 20/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
