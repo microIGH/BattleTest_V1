8dc18b968acd416139310d3e37f348a86471d611
ISRAEL GARCIA <micro4004@yahoo.com.mx>
2025-08-22 23:47:49 -0600

    Primer commit

---
+++ BattleTest_V1/ViewControlers/MainTabBarControllerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarControllerViewController.swift
+//  BattleTest_V1
+//
+//  Created by ISRAEL GARCIA on 20/08/25.
+//
+
+import UIKit
+
+class MainTabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
